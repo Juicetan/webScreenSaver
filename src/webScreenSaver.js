9d0a8d6f7f8b537a15a6c147b42b90890645375c
Justin <me@justinlam.ca>
2016-04-17 05:32:17 -0400

    Implemented basic video injection;

--- src/webScreenSaver.js
+++ src/webScreenSaver.js
@@ -13,8 +13,30 @@
     }
   };
 
+  var newVidCon = function(){
+    var vidCon = {
+      $:$("<video></video>",{
+        style: "min-width:100%;min-height:100%;width: auto;height: auto;position: fixed;top: 50%;left: 50%;transform: translate3d(-50%,-50%,0);",
+        class: 'vidcon',
+        autoplay: 'autoplay'
+      }),
+      setVideo: function(url){
+        var src = $("<source></source>",{
+          type: "video/webm",
+          src: url
+        });
+        this.$.html(src);
+      },
+    };
+
+    return vidCon;
+  };
+
   $['webScreenSaver'] = function(opts){
     config = $.extend({},defaults,opts);
+    var test = newVidCon();
+    $('body').append(test.$);
 
+    test.setVideo("http://screensaver.justinlam.ca/res/london-night.webm");
   };
 })(window,document,Math,jQuery);
