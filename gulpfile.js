0f4132a2adc6bc38e2db3b93a8decfdeabb51c05
Justin <me@justinlam.ca>
2016-04-24 15:35:54 -0400

    Setup dev work environment and test page;

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var es = require('event-stream');
+var runSync = require('run-sequence');
+var browserSync = require('browser-sync').create();
+
+var paths = {
+  startCode: './src/pluginStart.js',
+  endCode: './src/pluginEnd.js',
+  testPage: './test.html',
+  workingDir: './',
+  buildDir: './.tmp/',
+  builtDir: './dist/',
+  scripts: 'src/**/*.js',
+  updatePath: './dist/**/*.html'
+};
+
+var pluginName = "webScreenSaver.js";
+var pluginMinifiedName = "webScreenSaver.min.js";
+
+var startLiveReloadServer = function(){
+  browserSync.init({
+    open: false,
+    server: {
+      baseDir: 'dist'
+    }
+  });
+  console.log("> started live reload server");
+};
+
+var changeDetected = function(file){
+  console.log('> change detected',file);
+
+  runSync('combine',function(){
+    browserSync.reload();
+  });
+};
+
+gulp.task('clean',function(){
+  return es.concat(
+    gulp.src(paths.buildDir).pipe(clean()),
+    gulp.src(paths.builtDir).pipe(clean())
+  );
+});
+
+gulp.task('combine', function(){
+  var codePaths = [paths.startCode, paths.scripts, paths.endCode];
+
+  return es.concat(
+    gulp.src(codePaths)
+      .pipe(concat(pluginMinifiedName))
+      .pipe(uglify())
+      .pipe(gulp.dest(paths.builtDir)),
+    gulp.src(codePaths)
+      .pipe(concat(pluginName))
+      .pipe(gulp.dest(paths.builtDir)),
+    gulp.src(paths.testPage)
+      .pipe(rename('index.html'))
+      .pipe(gulp.dest(paths.builtDir))
+  );
+});
+
+gulp.task('watchChanges',function(){
+  startLiveReloadServer();
+
+  gulp.watch(paths.workingDir+paths.scripts,changeDetected);
+});
+
+gulp.task('serve',function(callback){
+  runSync('clean', 'combine', 'watchChanges' ,callback);
+});
--- src/webScreenSaver.js
+++ src/webScreenSaver.js
@@ -1,133 +1,130 @@
-;(function(window,document,Math,$,undefined){
-  var defaults = {
-    videos: [],
-    videoDuration: -1,
-    interval: 2*60*1000,
-    stagnantDelay: 5000,
-    stagnantTrigger: true
-  };
-  var config = {};
-  var currentVid = null;
-  var stagnantTimeout = null;
-  var isRunning = false;
+var defaults = {
+  videos: [],
+  videoDuration: -1,
+  interval: 2*60*1000,
+  stagnantDelay: 5000,
+  stagnantTrigger: true
+};
+var config = {};
+var currentVid = null;
+var stagnantTimeout = null;
+var isRunning = false;
 
-  var util = {
-    randomInt:function(min,max){
-      return Math.floor(Math.random()*(max-min+1)+min);
-    }
-  };
+var util = {
+  randomInt:function(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+};
 
-  var newVidCon = function(){
-    var vidCon = {
-      target: $('body'),
-      transitionDuration: 1000,
-      $:$("<video></video>",{
-        style: "min-width:100%;min-height:100%;width: auto;height: auto;position: fixed;top: 50%;left: 50%;transform: translate3d(-50%,-50%,0);opacity:0;",
-        class: 'vidcon',
-        autoplay: 'autoplay'
-      }),
-      setVideo: function(vidSrcObj){
-        var fragment = document.createDocumentFragment();
-        var extKeys = Object.keys(vidSrcObj);
+var newVidCon = function(){
+  var vidCon = {
+    target: $('body'),
+    transitionDuration: 1000,
+    $:$("<video></video>",{
+      style: "min-width:100%;min-height:100%;width: auto;height: auto;position: fixed;top: 50%;left: 50%;transform: translate3d(-50%,-50%,0);opacity:0;",
+      class: 'vidcon',
+      autoplay: 'autoplay'
+    }),
+    setVideo: function(vidSrcObj){
+      var fragment = document.createDocumentFragment();
+      var extKeys = Object.keys(vidSrcObj);
 
-        for(var i = 0; i < extKeys.length; i++){
-          (function(index){
-            var ext = extKeys[index];
-            var src = $("<source></source>",{
-              type: "video/"+ext,
-              src: vidSrcObj[ext]
-            });
-            fragment.appendChild(src[0]);
-          })(i);
-        }
-        this.$.html(fragment);
-      },
-      fadeIn: function(){
-        var def = $.Deferred();
-
-        this.target.append(this.$);
-        this.$.animate({
-          opacity: 1
-        },this.transitionDuration,function(){
-          def.resolve();
-        });
-
-        return def.promise();
-      },
-      fadeOut: function(){
-        var obj = this;
-        var def = $.Deferred();
-
-        this.$.animate({
-          opacity: 0
-        },this.transitionDuration,function(){
-          obj.$.remove();
-          def.resolve();
-        });
-
-        return def.promise();
+      for(var i = 0; i < extKeys.length; i++){
+        (function(index){
+          var ext = extKeys[index];
+          var src = $("<source></source>",{
+            type: "video/"+ext,
+            src: vidSrcObj[ext]
+          });
+          fragment.appendChild(src[0]);
+        })(i);
       }
-    };
+      this.$.html(fragment);
+    },
+    fadeIn: function(){
+      var def = $.Deferred();
 
-    return vidCon;
-  };
+      this.target.append(this.$);
+      this.$.animate({
+        opacity: 1
+      },this.transitionDuration,function(){
+        def.resolve();
+      });
 
-  var toggleVideos = function(){
-    var def = $.Deferred();
-    var randIndex = util.randomInt(0,config.videos.length-1);
-    var vidSrc = config.videos[randIndex];
+      return def.promise();
+    },
+    fadeOut: function(){
+      var obj = this;
+      var def = $.Deferred();
 
-    var tmpVid = newVidCon();
-    tmpVid.setVideo(vidSrc);
-    tmpVid.fadeIn().then(function(){
-      currentVid = tmpVid;
-      def.resolve(currentVid);
-    });
-    if(currentVid){
-      currentVid.fadeOut();
-    }
+      this.$.animate({
+        opacity: 0
+      },this.transitionDuration,function(){
+        obj.$.remove();
+        def.resolve();
+      });
 
-    return def.promise();
-  };
-
-  var startSaver = function(){
-    isRunning = true;
-    toggleVideos().then(function(newVid){
-
-    });
-  };
-
-  var stopSaver = function(){
-    isRunning = false;
-    if(currentVid){
-      currentVid.$.remove();
-      currentVid = null;
+      return def.promise();
     }
   };
 
-  var startWindowMonitor = function(){
+  return vidCon;
+};
+
+var toggleVideos = function(){
+  var def = $.Deferred();
+  var randIndex = util.randomInt(0,config.videos.length-1);
+  var vidSrc = config.videos[randIndex];
+
+  var tmpVid = newVidCon();
+  tmpVid.setVideo(vidSrc);
+  tmpVid.fadeIn().then(function(){
+    currentVid = tmpVid;
+    def.resolve(currentVid);
+  });
+  if(currentVid){
+    currentVid.fadeOut();
+  }
+
+  return def.promise();
+};
+
+var startSaver = function(){
+  isRunning = true;
+  toggleVideos().then(function(newVid){
+
+  });
+};
+
+var stopSaver = function(){
+  isRunning = false;
+  if(currentVid){
+    currentVid.$.remove();
+    currentVid = null;
+  }
+};
+
+var startWindowMonitor = function(){
+  stagnantTimeout = setTimeout(function(){
+    startSaver();
+  },config.stagnantDelay);
+
+  $(window).on('click mousemove mousedown keydown',function(){
+    window.clearTimeout(stagnantTimeout);
+    stopSaver();
     stagnantTimeout = setTimeout(function(){
       startSaver();
     },config.stagnantDelay);
+  });
+};
 
-    $(window).on('click mousemove mousedown keydown',function(){
-      window.clearTimeout(stagnantTimeout);
-      stopSaver();
-      stagnantTimeout = setTimeout(function(){
-        startSaver();
-      },config.stagnantDelay);
-    });
-  };
+$['webScreenSaver'] = function(opts){
+  config = $.extend({},defaults,opts);
 
-  $['webScreenSaver'] = function(opts){
-    config = $.extend({},defaults,opts);
+  stagnantTimeout = null;
+  if(config.stagnantTrigger){
+    startWindowMonitor();
+  }
 
-    stagnantTimeout = null;
-    if(config.stagnantTrigger){
-      startWindowMonitor();
-    }
-
-    return this;
-  };
-
-})(window,document,Math,jQuery);
+  return this;
+};
